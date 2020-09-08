8dc49852a66f04a3a8107e2a6d1fac5a148b435a
Ondřej Vele <ondrej.vele91@gmail.com>
2020-09-08 21:01:27 +0200

    Initial Commit

---
+++ Dice Roll WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Dice Roll WatchKit Extension
+//
+//  Created by Ondřej Vele on 08/09/2020.
+//  Copyright © 2020 Ondřej Vele. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
